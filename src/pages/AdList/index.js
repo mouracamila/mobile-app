5d82ab1e3b9c59d614ea5cf23aff834f5af0ba89
Camila Moura <camilapaleo@gmail.com>
2020-04-06 19:19:34 +0200

    create ad-list page with index and style

--- src/pages/AdList/index.js
+++ src/pages/AdList/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Feather from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
-import fotoImg from '../..assets/foto.png';
+// import fotoImg from '../..assets/foto.png';
 
 import styles from './styles';
 
 export default function AdList(){
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('AdDetail');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -21,31 +28,42 @@ export default function AdList(){
       <Text style={styles.description}>Entre em contato com o vendedor 
       clicando no anuncio </Text>
 
-      <View style={styles.announcementList}>
-        <View style={styles.announcement}>
-          <Image source={fotoImg} />
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.announcementList}
+        keyExtractor={announcement => String(announcement)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.announcementList}>
+            <View style={styles.announcement}>
+            {/* <Image source={fotoImg} /> */}
+            
+              <Text style={styles.announcementProperty}>Tomates</Text>
+            
+              <Text style={styles.announcementValue}>Lorem ipsum dolor sit amet 
+              consectetur, adipisicing elit. Aut, distinctio molestias accusamus, 
+              ab cupiditate aliquam laborum rerum voluptatem ducimus saepe harum 
+              explicabo at! Rem reprehenderit, necessitatibus corrupti quidem quos 
+              perferendis.</Text>
+              
+              <Text style={styles.announcementProperty}>VALOR:</Text>
+              <Text style={styles.announcementValue}>R$ 5,00</Text>
 
-          
-          <Text style={styles.announcementValue}>Tomates</Text>
+              <TouchableOpacity
+                style={styles.detailsButton}
+                onPress={navigateToDetail}
+              >
+                <Text style={styles.detailsButtonText}>Ver Mais detalhes</Text>
+                <Feather name="arrow-right" size={16} color="#518F57"/>
+              </TouchableOpacity>
 
-         
-          <Text style={styles.announcementValue}>Tomates</Text>
+            </View>
 
-          
-          <Text style={styles.announcementValue}>R$ 5,00</Text>
+          </View>
 
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => {}}
-          >
-            <Text style={styles.detailsButtonText}>Ver Mais detalhes</Text>
-            <Feather name="arrow-right" size={16} color="#518F57"/>
-          </TouchableOpacity>
+        )}
 
-
-        </View>
-
-      </View>
+      />
 
     </View>
   );
--- src/pages/AdList/styles.js
+++ src/pages/AdList/styles.js
@@ -24,9 +24,9 @@ export default StyleSheet.create({
   },
 
   title: {
-    fontSize: 30,
-    marginBottom: 16,
-    marginTop: 48,
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 20,
     color: '#13131a',
     fontWeight: 'bold',
   },
@@ -36,5 +36,42 @@ export default StyleSheet.create({
     lineHeight: 24,
     color: '#737380',
   },
+
+  announcementList: {
+    marginTop: 10,
+  },
+
+  announcement:{
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 2,
+  },
+
+  announcementProperty: {
+    fontSize: 16,
+    color: '#41414d',
+    fontWeight: 'bold',
+  },
+
+  announcementValue: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    color: '#737380'
+  },
+  
+  detailsButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  detailsButtonText: {
+    color: '#518F57',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
 
 });
