e2d58a01e31986b5320d6e588c85a90d1849c318
Camila Moura <camilapaleo@gmail.com>
2020-04-07 16:49:41 +0200

    add SellerProfile and clear code

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const AppStack = createStackNavigator();
 
-import Register from './pages/Register';
-import AdList from './pages/AdList';
-import AdDetail from './pages/AdDetail';
+import Register from "./pages/Register";
+import AdList from "./pages/AdList";
+import AdDetail from "./pages/AdDetail";
+import SellerProfile from "./pages/SellerProfile";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
-        
         <AppStack.Screen name="AdList" component={AdList} />
         <AppStack.Screen name="AdDetail" component={AdDetail} />
         <AppStack.Screen name="Register" component={Register} />
-      
+        <AppStack.Screen name="SellerProfile" component={SellerProfile} />
       </AppStack.Navigator>
-
     </NavigationContainer>
   );
 }
