f1b2e66988aac700f581708b8fa37c304383cb50
Camila Moura <camilapaleo@gmail.com>
2020-04-09 21:28:01 +0200

    create menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+export default function Menu() {
+  const navigation = useNavigation();
+
+  function navigateToFavoritesAd() {
+    navigation.navigate("FavoritesAd");
+  }
+
+  function navigateToMyAds() {
+    navigation.navigate("MyAds");
+  }
+
+  return (
+    <View style={styles.menu}>
+      <TouchableOpacity style={styles.menuIcon} onPress={navigateToMyAds}>
+        <Feather name="grid" size={20} color="#737380" />
+        <Text style={styles.menuText}>Anuncios</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuIcon}>
+        <Feather name="share" size={20} color="#737380" />
+        <Text style={styles.menuText}>Anunciar</Text>
+      </TouchableOpacity>
+      {/* <TouchableOpacity style={styles.menuIcon}>
+        <Feather name="settings" size={20} color="#737380" />
+        <Text style={styles.menuText}>Configurações</Text>
+      </TouchableOpacity> */}
+      <TouchableOpacity style={styles.menuIcon} onPress={navigateToFavoritesAd}>
+        <Feather name="heart" size={20} color="#737380" />
+        <Text style={styles.menuText}>Favoritos</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuIcon}>
+        <Feather name="user" size={20} color="#737380" />
+        <Text style={styles.menuText}>Conta</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+// import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  menu: {
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+
+  menuIcon: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  menuText: {
+    fontSize: 12,
+    color: "#737380",
+  },
+});
