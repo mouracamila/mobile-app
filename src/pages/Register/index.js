087b74712a41b2090ce3fc32a7842b59e85c43a0
Camila Moura <camilapaleo@gmail.com>
2020-04-09 14:14:29 +0200

    create register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,83 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, TextInput, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles";
+
+import logoImg from "../../assets/logo.png";
 
 // import background from "../../assets/background";
 
 export default function Register() {
+  const [username, setUsername] = React.useState("");
+  const [useremail, setUseremail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordRepeat, setPasswordRepeat] = React.useState("");
+
+  const navigation = useNavigation();
+
+  function navigateToList() {
+    navigation.navigate("AdList");
+  }
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Register</Text>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Text style={styles.actionText}> Voltar para Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.containerRegister}>
+        <Text style={styles.apresentation}>CRIANDO CONTA</Text>
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Name"
+          value={username}
+          onChangeText={setUsername}
+        />
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          value={useremail}
+          onChangeText={setUseremail}
+        />
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Criar Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Repitir Senha"
+          value={passwordRepeat}
+          onChangeText={setPasswordRepeat}
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          style={styles.actionRegister}
+          onPress={navigateToList}
+        >
+          <Text style={styles.actionTextregister}>CRIAR CONTA</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.actionText}> Aceitar Temos de uso</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -7,4 +7,58 @@ export default StyleSheet.create({
     paddingHorizontal: 16,
     paddingTop: Constants.statusBarHeight + 20,
   },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  actionText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginVertical: 5,
+    color: "#bbccb8",
+    marginTop: 24,
+  },
+
+  containerRegister: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+
+  apresentation: {
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "#bbccb8",
+    marginBottom: 24,
+  },
+
+  textInput: {
+    width: 300,
+    height: 50,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#518F57",
+    marginBottom: 10,
+    borderRadius: 16,
+  },
+
+  actionRegister: {
+    backgroundColor: "#518F57",
+    borderRadius: 20,
+    height: 50,
+    width: 300,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 24,
+  },
+
+  actionTextregister: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#ffff",
+  },
 });
