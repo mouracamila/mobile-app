d01194099c3e4f9edef38da7b9cc340c03f94cbc
Camila Moura <camilapaleo@gmail.com>
2020-04-09 21:31:11 +0200

    create myAds page

---
+++ src/pages/MyAds/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+import Announcement from "../../components/Announcement";
+import Menu from "../../components/Menu";
+import SellerData from "../../components/SellerData";
+
+export default function MyAds() {
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  const seller = {
+    name: "Tomateiro",
+  };
+
+  const announcements = [
+    {
+      id: 1,
+      title: "Tomate",
+      description: "Tomates verdes sem api",
+      price: 1.0,
+      price_by: "kg",
+    },
+    {
+      id: 2,
+      title: "Tomate",
+      description: "Tomates verdes sem api",
+      price: 1.0,
+      price_by: "kg",
+    },
+  ];
+
+  function navigateToDetail() {
+    navigation.navigate("AdDetail");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#737380" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather name="more-vertical" size={28} color="#737380" />
+        </TouchableOpacity>
+      </View>
+
+      <SellerData seller={seller} />
+
+      <Text style={styles.apresentation}>
+        Anuncios dos meus vendedores favoritos
+      </Text>
+      <FlatList
+        data={announcements}
+        // style={styles.announcementList}
+        keyExtractor={(announcement) => String(announcement.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={(announcement) => (
+          <View>
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+              onPress={() => navigateToDetail(navigateToDetail)}
+            >
+              <Announcement item={announcement} />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+      <Menu />
+    </View>
+  );
+}
---
+++ src/pages/MyAds/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#518F57",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingBottom: 10,
+  },
+
+  headerText: {
+    fontSize: 15,
+    color: "#737380",
+  },
+});
