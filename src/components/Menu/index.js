369d245fd6a0eeefcd64709d836a5a7ffe5837e6
Camila Moura <camilapaleo@gmail.com>
2020-04-10 21:21:50 +0200

    add in menu  navigate to  "createAd" and "account" page

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -16,25 +16,37 @@ export default function Menu() {
     navigation.navigate("MyAds");
   }
 
+  function navigateToCreateAd() {
+    navigation.navigate("CreateAd");
+  }
+
+  function navigateToAccount() {
+    navigation.navigate("Account");
+  }
+
   return (
     <View style={styles.menu}>
       <TouchableOpacity style={styles.menuIcon} onPress={navigateToMyAds}>
         <Feather name="grid" size={20} color="#737380" />
         <Text style={styles.menuText}>Anuncios</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.menuIcon}>
+
+      <TouchableOpacity style={styles.menuIcon} onPress={navigateToCreateAd}>
         <Feather name="share" size={20} color="#737380" />
         <Text style={styles.menuText}>Anunciar</Text>
       </TouchableOpacity>
+
       {/* <TouchableOpacity style={styles.menuIcon}>
         <Feather name="settings" size={20} color="#737380" />
         <Text style={styles.menuText}>Configurações</Text>
       </TouchableOpacity> */}
+
       <TouchableOpacity style={styles.menuIcon} onPress={navigateToFavoritesAd}>
         <Feather name="heart" size={20} color="#737380" />
         <Text style={styles.menuText}>Favoritos</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.menuIcon}>
+
+      <TouchableOpacity style={styles.menuIcon} onPress={navigateToAccount}>
         <Feather name="user" size={20} color="#737380" />
         <Text style={styles.menuText}>Conta</Text>
       </TouchableOpacity>
