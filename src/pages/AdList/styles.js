e68431c096fba9869b85e4108e960022854fe5db
Camila Moura <camilapaleo@gmail.com>
2020-04-19 17:06:39 +0200

    simplificate ui in AdList

--- src/pages/AdList/styles.js
+++ src/pages/AdList/styles.js
@@ -4,48 +4,14 @@ import Constants from "expo-constants";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    // paddingHorizontal: 16,
-    paddingTop: Constants.statusBarHeight + 20,
+    paddingHorizontal: 16,
+    paddingTop: Constants.statusBarHeight,
   },
 
   header: {
+    paddingHorizontal: 16,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-  },
-
-  headerText: {
-    fontSize: 15,
-    color: "#737380",
-  },
-
-  headerTextBold: {
-    fontWeight: "bold",
-  },
-
-  title: {
-    fontSize: 20,
-    marginBottom: 10,
-    marginTop: 20,
-    color: "#13131a",
-    fontWeight: "bold",
-  },
-
-  description: {
-    fontSize: 16,
-    // lineHeight: 24,
-    color: "#737380",
-  },
-
-  detailsButton: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  detailsButtonText: {
-    color: "#518F57",
-    fontSize: 15,
-    fontWeight: "bold",
   },
 });
